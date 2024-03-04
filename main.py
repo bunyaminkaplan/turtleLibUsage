a001a5c61b4a19cdf9c993f42aaec2e7f17d9772
Bunyamin Kaplan <bunyaminn.kaplan@gmail.com>
2024-03-04 14:29:52 +0000

    created a square shape with turtle

--- main.py
+++ main.py
@@ -6,5 +6,11 @@ drawing_board.title("Turtle Test")
 
 turtle_instance = turtle.Turtle()
 turtle_instance.forward(100)
+turtle_instance.left(90)
+turtle_instance.forward(100)
+turtle_instance.left(90)
+turtle_instance.forward(100)
+turtle_instance.left(90)
+turtle_instance.forward(100)
 
 turtle.done()
